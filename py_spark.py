fcb7c4061f63f63d3b4cd2879c1f4184bafe1851
rrazumilovs <razumilovrenat@inbox.lv>
2022-06-02 11:12:12 +0300

    refactor

--- py_spark.py
+++ py_spark.py
@@ -1,6 +1,6 @@
 import pathlib
 from pyspark.sql import SparkSession
-from pyspark.sql import functions as func
+from pyspark.sql.functions import when, col, lit, regexp_replace, split, count, round, avg
 from pyspark.sql.types import IntegerType as Int
 
 address_in = pathlib.Path('.').absolute() / 'files/raw'
@@ -19,9 +19,9 @@ split_street, region, floor_split, top_floor, com_type, price_refined, top_sell_
 
 
 def categorize(column_price, price_contains, cat_type, other_col=None):
-    when_clause = func.when(func.col(column_price).contains(price_contains), cat_type)
+    when_clause = when(col(column_price).contains(price_contains), cat_type)
     if other_col is not None:
-        when_clause = when_clause.otherwise(func.col(other_col))
+        when_clause = when_clause.otherwise(col(other_col))
     return when_clause
 
 
@@ -31,9 +31,9 @@ def show_dataframe(dataframe, record_count=None):
     for cat in categories:
         print('category: ', cat)
         if record_count is not None:
-            dataframe.where(func.col(com_type) == cat).show(record_count)
+            dataframe.where(col(com_type) == cat).show(record_count)
         else:
-            dataframe.where(func.col(com_type) == cat).show()
+            dataframe.where(col(com_type) == cat).show()
     dataframe.printSchema()
 
 
@@ -52,35 +52,34 @@ def set_categories(data_frame):
     df_want = df_rent_day.withColumn(com_type, categorize(price, 'vēlosīret', want_2_rent, other_col=com_type))
     df_buy = df_want.withColumn(com_type, categorize(price, 'pērku', buy, other_col=com_type))
     df_chng = df_buy.withColumn(com_type, categorize(price, 'maiņai', change, other_col=com_type))
-    return df_chng.withColumn(com_type,
-                              func.when(df_chng.com_type.isNull(), func.lit('other')).otherwise(df_chng.com_type))
+    return df_chng.withColumn(com_type, when(df_chng.com_type.isNull(), lit('other')).otherwise(df_chng.com_type))
 
 
 def clean_price(data_frame):
-    df_ref1 = data_frame.withColumn(price_refined, func.regexp_replace(func.col(price), "€", ""))
-    df_ref2 = df_ref1.withColumn(price_refined, func.regexp_replace(func.col(price_refined), "/mēn.", ""))
-    df_ref3 = df_ref2.withColumn(price_refined, func.regexp_replace(func.col(price_refined), "/dienā", ""))
-    df_ref4 = df_ref3.withColumn(price_refined, func.regexp_replace(func.col(price_refined), "maiņai", ""))
-    df_ref5 = df_ref4.withColumn(price_refined, func.regexp_replace(func.col(price_refined), "pērku", ""))
-    df_ref6 = df_ref5.withColumn(price_refined, func.regexp_replace(func.col(price_refined), "vēlosīret", ""))
-    return df_ref6.withColumn(price_refined, func.regexp_replace(func.col(price_refined), ",", ""))
+    df_ref1 = data_frame.withColumn(price_refined, regexp_replace(col(price), "€", ""))
+    df_ref2 = df_ref1.withColumn(price_refined, regexp_replace(col(price_refined), "/mēn.", ""))
+    df_ref3 = df_ref2.withColumn(price_refined, regexp_replace(col(price_refined), "/dienā", ""))
+    df_ref4 = df_ref3.withColumn(price_refined, regexp_replace(col(price_refined), "maiņai", ""))
+    df_ref5 = df_ref4.withColumn(price_refined, regexp_replace(col(price_refined), "pērku", ""))
+    df_ref6 = df_ref5.withColumn(price_refined, regexp_replace(col(price_refined), "vēlosīret", ""))
+    return df_ref6.withColumn(price_refined, regexp_replace(col(price_refined), ",", ""))
 
 
 def set_top_floor(data_frame):
-    df_top_floor_split = data_frame.withColumn(floor_split, func.split(data_frame.floor, "/"))
+    df_top_floor_split = data_frame.withColumn(floor_split, split(data_frame.floor, "/"))
     return df_top_floor_split.withColumn(top_floor, df_top_floor_split.floor_split.getItem(1)) \
         .withColumn(floor, df_top_floor_split.floor_split.getItem(0))
 
 
 def set_region_and_street(data_frame):
-    df_split = data_frame.withColumn(split_street, func.split(data_frame.street, "::"))
+    df_split = data_frame.withColumn(split_street, split(data_frame.street, "::"))
     return df_split.withColumn(region, df_split.split_street.getItem(0)) \
         .withColumn(street, df_split.split_street.getItem(1))
 
 
 def commercials_by_house_type(data_frame):
     return data_frame.select(house_type) \
-        .withColumn(house_type, func.regexp_replace(func.col(house_type), '-', 'Unspecified')) \
+        .withColumn(house_type, regexp_replace(col(house_type), '-', 'Unspecified')) \
         .groupby(house_type) \
         .count() \
         .sort('count', ascending=False)
@@ -94,13 +93,13 @@ def commercials_by_category(data_frame):
 
 
 def top_zones_by_commercial_count(data_frame):
-    return data_frame.select(region).groupby(region).agg(func.count(region).alias('counts')) \
+    return data_frame.select(region).groupby(region).agg(count(region).alias('counts')) \
         .sort('counts', ascending=False)
 
 
 def average_price_by_category(dataframe, category):
-    return dataframe.filter(func.col(com_type).eqNullSafe(category)).groupby(region) \
-        .agg(func.round(func.avg(price_refined), 2).alias(price_refined)).sort(price_refined, ascending=False)
+    return dataframe.filter(col(com_type).eqNullSafe(category)).groupby(region) \
+        .agg(round(avg(price_refined), 2).alias(price_refined)).sort(price_refined, ascending=False)
 
 
 def top_floors(data_frame):
@@ -108,8 +107,8 @@ def top_floors(data_frame):
 
 
 def count_selling_floors(data_frame, category):
-    return data_frame.select(com_type, floor).filter(func.col(com_type).eqNullSafe(category)).groupby(floor)\
-        .agg(func.count(floor).alias('sell_counts')).sort('sell_counts', ascending=False)
+    return data_frame.select(com_type, floor).filter(col(com_type).eqNullSafe(category)).groupby(floor)\
+        .agg(count(floor).alias('sell_counts')).sort('sell_counts', ascending=False)
 
 
 def main():
@@ -128,13 +127,13 @@ def main():
     df_other = set_categories(df_top_floor)
 
     # remove hidden symbols
-    df_desc = df_other.withColumn(description, func.regexp_replace(func.col(description), "\n", ""))
+    df_desc = df_other.withColumn(description, regexp_replace(col(description), "\n", ""))
 
     # clean price
     df_refined = clean_price(df_desc)
 
     # cast numeric
-    df_numeric = df_refined.withColumn(price_refined, func.col(price_refined).cast(Int()))
+    df_numeric = df_refined.withColumn(price_refined, col(price_refined).cast(Int()))
     df_numeric.cache()
 
     # commercial by house type
