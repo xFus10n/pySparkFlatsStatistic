e4a049a36833f7e1149726b1b6bce7085d62e885
rrazumilovs <razumilovrenat@inbox.lv>
2022-06-02 10:59:12 +0300

    test++

--- pyTest.py
+++ pyTest.py
@@ -2,8 +2,6 @@ import logging
 import pytest
 import py_spark as main
 from pyspark_test import assert_pyspark_df_equal
-# from pyspark import SparkConf
-# from pyspark import SparkContext
 from pyspark.sql import SparkSession
 
 
@@ -13,34 +11,12 @@ def quiet_py4j():
     logger.setLevel(logging.WARN)
 
 
-# @pytest.fixture(scope="session")
-# def spark_context(request):
-#     conf = (SparkConf().setMaster("local[*]").setAppName("pytest-pyspark-local-testing"))
-#     request.addfinalizer(lambda: sc.stop())
-#     sc = SparkContext(conf=conf)
-#     quiet_py4j()
-#     return sc
-
-
 @pytest.fixture(scope="session")
 def spark_session(request):
     spark_session = SparkSession.builder.getOrCreate()
     request.addfinalizer(lambda: spark_session.stop())
     quiet_py4j()
     return spark_session
-
-
-'''EXAMPLES:'''
-
-
-# @pytest.fixture(scope="session")
-# def hive_context(spark_context):
-#     return HiveContext(spark_context)
-#
-#
-# @pytest.fixture(scope="session")
-# def streaming_context(spark_context):
-#     return StreamingContext(spark_context, 1)
 
 
 def test_top_floor_extract(spark_session):
@@ -69,11 +45,27 @@ def test_region_extract(spark_session):
     assert_pyspark_df_equal(expected_df, actual_df)
 
 
+def test_categorization(spark_session):
+    # arrange
+    test_df = get_data_frame(spark_session)
+    expected_df = get_output_for_categorization(spark_session)
+
+    # act
+    output_df = main.set_categories(test_df)
+    actual_df = output_df.select(main.com_type)
+
+    # assert
+    assert_pyspark_df_equal(expected_df, actual_df)
+
+
 def get_data_frame(spark_session):
     columns = main.original_columns
     data = [('link1', 'desc1', 'centrs::Valdemāra 106', '3', '72', '1/5', 'Staļina', '105,000  €'),
-            ('link2', 'desc2', 'Zolitūde::Lejiņa 18', '3', '74', '10/10', '119.', '96,000  €'),
-            ('link3', 'desc3', 'Jugla::Murjāņu 52', '2', '44', '1/5', 'Hrušč.', '44,000  €')]
+            ('link2', 'desc2', 'Zolitūde::Lejiņa 18', '3', '74', '10/10', '119.', 'vēlosīret'),
+            ('link3', 'desc3', 'Jugla::Murjāņu 52', '2', '44', '1/5', 'Hrušč.', '250  €/mēn'),
+            ('link4', 'desc4', 'centrs::Matīsa 41', '1', '20', '1/2', 'Renov.', '30  €/dienā'),
+            ('link5', 'desc5', 'Āgenskalns::', '3', '65', '-', 'P. kara', 'pērku'),
+            ('link5', 'desc5', 'Pļavnieki::', '2', '-', '-', '602.', 'maiņai')]
     return spark_session.createDataFrame(data).toDF(*columns)
 
 
@@ -81,7 +73,10 @@ def get_output_for_region_extract(spark_session):
     columns = (main.region, main.street)
     data = [('centrs', 'Valdemāra 106'),
             ('Zolitūde', 'Lejiņa 18'),
-            ('Jugla', 'Murjāņu 52')]
+            ('Jugla', 'Murjāņu 52'),
+            ('centrs', 'Matīsa 41'),
+            ('Āgenskalns', ''),
+            ('Pļavnieki', '')]
     return spark_session.createDataFrame(data).toDF(*columns)
 
 
@@ -89,5 +84,14 @@ def get_output_for_floor_extract(spark_session):
     columns = (main.floor, main.top_floor)
     data = [('1', '5'),
             ('10', '10'),
-            ('1', '5')]
+            ('1', '5'),
+            ('1', '2'),
+            ('-', None),
+            ('-', None)]
+    return spark_session.createDataFrame(data).toDF(*columns)
+
+
+def get_output_for_categorization(spark_session):
+    columns = (main.com_type,)
+    data = [(main.sell,), (main.want_2_rent,), (main.rent,), (main.rent_by_day,), (main.buy,), (main.change,)]
     return spark_session.createDataFrame(data).toDF(*columns)
